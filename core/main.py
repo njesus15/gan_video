afc448330688db197f28b7d355633619b4e2c59f
Jesus Navarro <navarrojes.me@gmail.com>
2019-08-13 12:54:08 -0700

    ignore dir /runs

--- core/main.py
+++ core/main.py
@@ -1,7 +1,7 @@
 from keras_preprocessing.image import save_img
 from torch import optim
 from torch.utils.data import DataLoader
-from utils import *
+from core.utils import *
 from torchvision.transforms import ToPILImage
 import logging
 from core.model import *
@@ -10,49 +10,6 @@ from examples.Logger import Logger
 import sys
 import imageio
 from skimage import img_as_ubyte
-
-
-def make_gif(images, filename):
-    # Receives [3,32,64,64] tensor, and creates a gif
-
-    x = images.permute(1,2,3,0)
-    x = x.numpy()
-    x = img_as_ubyte(x)
-    frames = []
-    for i in range(32):
-        frames += [x[i]]
-    imageio.mimsave(filename, frames, duration=0.1)
-
-def denorm(x):
-    out = (x + 1.0) / 2.0
-    tf = nn.Tanh()
-    return tf(out)
-
-
-def setup_logger(logger_name):
-    # format string
-    LOG_FORMAT = "%(levelname)s - %(asctime)s - %(messages)s"
-    logger = logging.getLogger(logger_name)
-
-    # create formatter
-    formatter = logging.Formatter(fmt=LOG_FORMAT)
-
-    # create handler, streamhandler, and format
-    file_handler = logging.FileHandler('training_log.txt', mode='w')
-    file_handler.setFormatter(file_handler)
-    screen_handler = logging.StreamHandler(stream=sys.stdout)
-    screen_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    logger.addHandler(screen_handler)
-
-    return logger
-
-
-def save_image(img, filename):
-    img = img.detach()
-    pil_img = ToPILImage()
-    img = pil_img(img)
-    img.save(filename)
 
 
 if __name__ == '__main__':
